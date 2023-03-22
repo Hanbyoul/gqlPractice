825a9a0293dae09368fa90837dfd57cafe2fc1bf
Hanbyoul <dytjqdla120@naver.com>
2023-03-22 18:04:53 +0900

    rest gql migration

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 let tweets = [
   {
@@ -31,9 +32,14 @@ const typeDefs = gql`
     id: ID!
     firstName: String!
     lastName: String!
+    """
+    Is the sum of firstName + lastName as a string
+    """
     fullName: String!
   }
-
+  """
+  Tweet object represents a resource for a Tweet
+  """
   type Tweet {
     id: ID!
     text: String!
@@ -41,13 +47,42 @@ const typeDefs = gql`
   }
 
   type Query {
+    allMovies: [Movie!]!
     allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    movie(id: String!): Movie
   }
+
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Delete a Tweet if found, else returns false
+    """
     deleteTweet(id: ID!): Boolean!
+  }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Int!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String!
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
@@ -62,6 +97,18 @@ const resolvers = {
     allUsers() {
       console.log("allUsers Called");
       return users;
+    },
+    allMovies() {
+      return fetch("https://yts.torrentbay.to/api/v2/list_movies.json")
+        .then((res) => res.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(
+        `https://yts.torrentbay.to/api/v2/movie_details.json?movie_id=${id}`
+      )
+        .then((res) => res.json())
+        .then((json) => json.data.movie);
     },
   },
   Mutation: {
